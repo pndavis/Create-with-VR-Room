d74454841dad00c23ec028696f09f478c576a51b
Paul Davis <paul@pauldavis.io>
2023-03-20 16:51:08 -0700

    First commit

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine;
+
+public class SpawnManager : XRBaseInteractable
+{
+    private GameObject grabbableObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
